92af5c7af2c2ac98ebea8f7b2ff164acd254bdf3
phantu279999 <phantu279999@gmail.com>
2024-09-20 20:22:22 +0700

    update code

--- BlangoPro/settings.py
+++ BlangoPro/settings.py
@@ -20,13 +20,13 @@ class Dev(Configuration):
 
 	ALLOWED_HOSTS = ["*"]
 
-	# X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
-	# CSRF_COOKIE_SAMESITE = None
-	# CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
-	# CSRF_COOKIE_SECURE = True
-	# SESSION_COOKIE_SECURE = True
-	# CSRF_COOKIE_SAMESITE = 'None'
-	# SESSION_COOKIE_SAMESITE = 'None'
+	X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
+	CSRF_COOKIE_SAMESITE = None
+	CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
+	CSRF_COOKIE_SECURE = True
+	SESSION_COOKIE_SECURE = True
+	CSRF_COOKIE_SAMESITE = 'None'
+	SESSION_COOKIE_SAMESITE = 'None'
 
 	# Application definition
 
@@ -54,10 +54,10 @@ class Dev(Configuration):
 		'django.middleware.security.SecurityMiddleware',
 		'django.contrib.sessions.middleware.SessionMiddleware',
 		'django.middleware.common.CommonMiddleware',
-		'django.middleware.csrf.CsrfViewMiddleware',
+		# 'django.middleware.csrf.CsrfViewMiddleware',
 		'django.contrib.auth.middleware.AuthenticationMiddleware',
 		'django.contrib.messages.middleware.MessageMiddleware',
-		'django.middleware.clickjacking.XFrameOptionsMiddleware',
+		# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	]
 
 	ROOT_URLCONF = 'BlangoPro.urls'
